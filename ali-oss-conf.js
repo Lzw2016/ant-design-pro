e421cb8da1a7bb26f7e98be9bec9288f13615622
lizw <lzw1000000@163.com>
2019-11-13 15:46:09 +0800

    oss 配置

--- ali-oss-conf.js
+++ ali-oss-conf.js
@@ -4,13 +4,13 @@ const aliOssConf = {
   region: 'oss-cn-hangzhou',
   accessKeyId: ignore.accessKeyId,
   accessKeySecret: ignore.accessKeySecret,
-  bucket: 'ant-msvc-top',
-  ossUrl: 'https://ant-msvc-top.oss-cn-hangzhou.aliyuncs.com',
+  bucket: 'clever-security',
+  ossUrl: 'https://clever-security.oss-cn-hangzhou.aliyuncs.com',
   // oss 使用域名绑定之后变成CND
-  cdnUrl: 'http://cdn.ant.msvc.top',
+  cdnUrl: 'http://cdn.security.msvc.top',
 };
 
 module.exports = {
-  appVersion: 'v1.0.3',
+  appVersion: 'v1.0.0',
   ...aliOssConf,
 };
